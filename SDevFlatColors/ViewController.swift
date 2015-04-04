73b84f44df86767023bfa98248b0dd0fac126c71
Sedat CIFTCI <me@sedat.us>
2015-04-04 15:23:30 +0300

    Initial Commit

--- SDevFlatColors/ViewController.swift
+++ SDevFlatColors/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = UIColor.flat(FlatColors.Wisteria)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
